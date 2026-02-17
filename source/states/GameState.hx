d032b14eb1bd6e0d2ac21e4268369f337bda360d
Sibuxiangtail <callumgrimmy@gmail.com>
2026-02-17 19:34:17 +0000

    initial(crashes)

--- source/states/GameState.hx
+++ source/states/GameState.hx
@@ -1,30 +1,17 @@
 package states;
 
+import SaveSystem;
 import flixel.FlxState;
-import flixel.FlxText;
 import flixel.addons.ui.FlxUIButton;
-import flixel.addons.ui.FlxUITypedButton;
-import flixel.addons.ui.FlxUICheckBox;
-import flixel.addons.ui.FlxUICursor;
-import source.SaveSystem;
-import math;
 
 
-class PlayState extends FlxState
+class GameState extends FlxState
 {     
-   //delta time
-   var dt = math.min(elapsed,0.1);
 
-   public function create():Void
+
+   override public function create():Void
     {
-        _xmd_id = "Menus";
         super.create();
-   }
-
-   public function updateMoney():Void
-   {
-         var money = money + incomePerSecond * dt;
-
    }
 
 
